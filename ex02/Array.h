d5448763763b42a7d38bc867a76c0c32c2164d25
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2026-03-19 18:00:55 +0000

    final test

---
+++ ex02/Array.h
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/19 17:30:58 by htrindad          #+#    #+#             */
+/*   Updated: 2026/03/19 17:59:14 by htrindad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <exception>
+#include <cstdlib>
+
+template <typename T>
+class Array
+{
+	private:
+		T *arr;
+		std::size_t size;
+	public:
+		Array() : arr(new T[0]), size(0) {}
+		Array(std::size_t len) : arr(new T[len]), size(len) {}
+		~Array() { delete[] T; }
+		Array(const Array &ref) : arr(new T[ref.size]), size(ref.size)
+		{
+			for (std::size_t i = 0; i < size; i++)
+				arr[i] = ref.arr[i];
+		}
+		Array &operator=(const Array &ref)
+		{
+			if (this != ref)
+			{
+				delete[] arr;
+				size = ref.size;
+				arr = new T[size];
+				for (std::size_t i = 0; i < size; i++)
+					arr[i] = ref.arr[i];
+			}
+			return *this;
+		}
+		const T &operator[](std::size_t i)
+		{
+			if (i >= size)
+				throw std::out_of_range("out of bounds");
+			return arr[i];
+		}
+		T &operator[](std::size_t i) { return const_cast<T&>(static_cast<const Array&>(*this)[i]); }
+};
