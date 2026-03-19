8c959e5ace185b5c19c8414484206efe49edf1a5
Hugo Miguel Trindade Parada <htrindad@student.42lisboa.com>
2026-03-19 18:04:00 +0000

    fixes needed

--- ex02/Array.h
+++ ex02/Array.h
@@ -6,7 +6,7 @@
 /*   By: htrindad <htrindad@student.42lisboa.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/19 17:30:58 by htrindad          #+#    #+#             */
-/*   Updated: 2026/03/19 17:59:14 by htrindad         ###   ########.fr       */
+/*   Updated: 2026/03/19 18:03:31 by htrindad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,6 @@ class Array
 	public:
 		Array() : arr(new T[0]), size(0) {}
 		Array(std::size_t len) : arr(new T[len]), size(len) {}
-		~Array() { delete[] T; }
 		Array(const Array &ref) : arr(new T[ref.size]), size(ref.size)
 		{
 			for (std::size_t i = 0; i < size; i++)
@@ -41,11 +40,12 @@ class Array
 			}
 			return *this;
 		}
-		const T &operator[](std::size_t i)
+		const T &operator[](std::size_t i) const
 		{
 			if (i >= size)
 				throw std::out_of_range("out of bounds");
 			return arr[i];
 		}
 		T &operator[](std::size_t i) { return const_cast<T&>(static_cast<const Array&>(*this)[i]); }
+		~Array() { delete[] arr; }
 };
